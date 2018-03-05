bee0362eb51178515aa8e2787fb726b0a61664a2
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2018-03-05 15:56:43 +0200

    Start using opcode numbers from wasm bytecode.

--- src/rs-3/x64-linux/vm.s
+++ src/rs-3/x64-linux/vm.s
@@ -98,25 +98,73 @@ macro def_op opcode {
     store qword ($ - $$) at op.table + opcode * 8
 }
 
-def_op 0x00 ; NOP
+def_op 0x00 ; unreachable
+    mov rsi, msg.trap_op
+    mov rdx, msg.trap_op.len
+    call vm.panic
+
+def_op 0x01 ; nop
     jmp vm.loop
 
-def_op 0x01 ; LIT32      S.push(varuint32)
-    call imm.varint32
-    S.push eax
-    jmp vm.loop
-
-def_op 0x02 ; CALL       R.push(IP + 1), IP = S.pop()
-    R.push vm.IP
-    S.pop vm.IP
-    jmp op.jmp.check
-
-def_op 0x03 ; RET        IP = R.pop()
+def_op 0x0f ; return
 op.ret:
     R.pop vm.IP
     jmp op.jmp.check
 
-def_op 0x04 ; JMP        IP = S.pop()
+def_op 0x11 ; call_indirect
+    R.push vm.IP
+    S.pop vm.IP
+    jmp op.jmp.check
+
+def_op 0x41 ; i32.const
+    call imm.varint32
+    S.push eax
+    jmp vm.loop
+
+; S.push(f(S.pop(), S.pop()))
+macro def_op.i32.binop opcode, op {
+    def_op opcode
+        S.pop eax
+        op [vm.SP], eax
+        jmp vm.loop
+}
+
+def_op.i32.binop 0x6a, add ; i32.add
+def_op.i32.binop 0x6b, sub ; i32.sub
+
+def_op 0x6c ; i32.mul
+    S.pop eax
+    imul eax, [vm.SP]
+    mov [vm.SP], eax
+    jmp vm.loop
+
+; div(S.pop(), S.pop()); S.push(out)
+macro def_op.i32.divrem opcode, div, out {
+    def_op opcode
+        S.pop ecx ; divisor
+        mov eax, [vm.SP] ; dividend
+        if div eq idiv
+            cdq ; edx:eax = sext eax
+        else
+            xor edx, edx
+        end if
+
+        div ecx
+        mov [vm.SP], out
+        jmp vm.loop
+}
+
+def_op.i32.divrem 0x6d, idiv, eax ; i32.div_s
+def_op.i32.divrem 0x6e, div, eax ; i32.div_u
+def_op.i32.divrem 0x6f, idiv, edx ; i32.rem_s
+def_op.i32.divrem 0x70, div, edx ; i32.rem_u
+
+def_op.i32.binop 0x71, and ; i32.and
+def_op.i32.binop 0x72, or ; i32.or
+def_op.i32.binop 0x73, xor ; i32.xor
+
+; Legacy (pre-WASM) RS-3 opcodes
+def_op 0xf0 ; JMP        IP = S.pop()
     S.pop vm.IP
 op.jmp.check:
     cmp vm.IP, 0
@@ -142,7 +190,7 @@ op.jmp.builtin.undef:
     mov rdx, msg.undef_builtin.len
     jmp vm.panic
 
-def_op 0x05 ; CMP        S.push([S.pop(), S.pop(), S.pop()][S.pop().cmp(S.pop())])
+def_op 0xf1 ; CMP        S.push([S.pop(), S.pop(), S.pop()][S.pop().cmp(S.pop())])
     ; Stack: [a, b, lt, eq, gt] <- SP
     add vm.SP, 4 * 4 ; Point SP to a
     mov eax, [vm.SP]
@@ -153,59 +201,17 @@ def_op 0x05 ; CMP        S.push([S.pop(), S.pop(), S.pop()][S.pop().cmp(S.pop())
     mov [vm.SP], eax
     jmp vm.loop
 
-def_op 0x06 ; GET        S.push(S[varuint32])
+def_op 0xf2 ; GET        S.push(S[varuint32])
     call imm.varuint32
     mov eax, [vm.SP + rax * 4]
     S.push eax
     jmp vm.loop
 
-def_op 0x07 ; SET        S[varuint32] = S.pop()
+def_op 0xf3 ; SET        S[varuint32] = S.pop()
     call imm.varuint32
     S.pop ecx
     mov [vm.SP + rax * 4], ecx
     jmp vm.loop
-
-; S.push(f(S.pop(), S.pop()))
-macro def_binop opcode, op {
-    def_op opcode
-        S.pop eax
-        op [vm.SP], eax
-        jmp vm.loop
-}
-
-def_binop 0x10, add
-def_binop 0x11, sub
-
-def_op 0x12
-    S.pop eax
-    imul eax, [vm.SP]
-    mov [vm.SP], eax
-    jmp vm.loop
-
-; div(S.pop(), S.pop()); S.push(out)
-macro def_binop.divrem opcode, div, out {
-    def_op opcode
-        S.pop ecx ; divisor
-        mov eax, [vm.SP] ; dividend
-        if div eq idiv
-            cdq ; edx:eax = sext eax
-        else
-            xor edx, edx
-        end if
-
-        div ecx
-        mov [vm.SP], out
-        jmp vm.loop
-}
-
-def_binop.divrem 0x13, idiv, eax
-def_binop.divrem 0x14, div, eax
-def_binop.divrem 0x15, idiv, edx
-def_binop.divrem 0x16, div, edx
-
-def_binop 0x17, and
-def_binop 0x18, or
-def_binop 0x19, xor
 
 op.ud:
     mov rsi, msg.undef_op
@@ -247,6 +253,9 @@ vm.loop:
     mov rcx, [r11 + (op.table - $$) + rbx * 8]
     add rcx, r11
     jmp rcx
+
+msg.trap_op: db "Trap instruction", 10
+msg.trap_op.len = $ - msg.trap_op
 
 msg.undef_op: db "Undefined instruction", 10
 msg.undef_op.len = $ - msg.undef_op
